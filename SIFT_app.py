771724247fc86cf2e48c7475cbf32ba3964b3113
wecassidy <wec.notif.github@gmail.com>
2020-09-30 19:43:34 -0700

    Version 1: mostly kinda sorta working

---
+++ SIFT_app.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, division
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from python_qt_binding import loadUi
+
+import cv2
+import numpy as np
+import sys
+
+
+class SIFTApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(SIFTApp, self).__init__()
+        loadUi("SIFT_app.ui", self)
+
+        self._cam_id = 0
+        self._cam_fps = 2
+        self._is_cam_enabled = False
+        self._is_template_loaded = False
+
+        self.browse_button.clicked.connect(self.SLOT_browse_button)
+        self.toggle_cam_button.clicked.connect(self.SLOT_toggle_camera)
+
+        self._camera_device = cv2.VideoCapture(self._cam_id)
+        self._camera_device.set(3, 320)
+        self._camera_device.set(4, 240)
+
+        # Timer used to trigger the camera
+        self._timer = QtCore.QTimer(self)
+        self._timer.timeout.connect(self.SLOT_query_camera)
+        self._timer.setInterval(1000 / self._cam_fps)
+
+        self.MIN_MATCH_COUNT = 10
+        self.sift = cv2.xfeatures2d.SIFT_create()
+        self.template_keypoints = None
+        self.template_descriptors = None
+
+    def SLOT_browse_button(self):
+        dialogue = QtWidgets.QFileDialog()
+        dialogue.setFileMode(QtWidgets.QFileDialog.ExistingFile)
+        if dialogue.exec_():
+            self.template_path = dialogue.selectedFiles()[0]
+        else:
+            return
+
+        self.template = cv2.imread(self.template_path)
+        grey = cv2.cvtColor(self.template, cv2.COLOR_BGR2GRAY)
+        self.template_keypoints, self.template_descriptors = self.sift.detectAndCompute(
+            grey, None
+        )
+
+        pixmap = QtGui.QPixmap(self.template_path)
+        self.template_label.setPixmap(pixmap)
+        print("Loaded template image file: " + self.template_path)
+
+    def convert_cv_to_pixmap(self, cv_img):
+        cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        height, width, channel = cv_img.shape
+        bytesPerLine = channel * width
+        q_img = QtGui.QImage(
+            cv_img.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888
+        )
+        return QtGui.QPixmap.fromImage(q_img)
+
+    def SLOT_query_camera(self):
+        ret, frame = self._camera_device.read()
+
+        grey = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        keypoints, descriptors = self.sift.detectAndCompute(grey, None)
+
+        if (
+            self.template_keypoints is not None
+            and self.template_descriptors is not None
+        ):
+            FLANN_INDEX_KDTREE = 1
+            matcher = cv2.FlannBasedMatcher(
+                {"algorithm": FLANN_INDEX_KDTREE, "trees": 5}, {"checks": 50}
+            )
+            matches = matcher.knnMatch(self.template_descriptors, descriptors, k=2)
+
+            good = [m for m, n in matches if m.distance < 0.7 * n.distance]
+            matchesMask = None
+            if len(good) > self.MIN_MATCH_COUNT:
+                src_pts = np.float32(
+                    [self.template_keypoints[m.queryIdx].pt for m in good]
+                ).reshape(-1, 1, 2)
+                dst_pts = np.float32([keypoints[m.trainIdx].pt for m in good]).reshape(
+                    -1, 1, 2
+                )
+
+                M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+                matchesMask = mask.ravel().tolist()
+
+                h, w, _ = self.template.shape
+                box = np.float32(
+                    [[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]
+                ).reshape(-1, 1, 2)
+                dst = cv2.perspectiveTransform(box, M)
+                frame = cv2.polylines(frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+
+            draw_params = dict(
+                matchColor=(0, 255, 0),  # draw matches in green color
+                singlePointColor=None,
+                matchesMask=matchesMask,  # draw only inliers
+                flags=2,
+            )
+            frame = cv2.drawMatches(
+                self.template,
+                self.template_keypoints,
+                frame,
+                keypoints,
+                good,
+                None,
+                **draw_params
+            )
+
+        pixmap = self.convert_cv_to_pixmap(frame)
+        self.live_image_label.setPixmap(pixmap)
+
+    def SLOT_toggle_camera(self):
+        if self._is_cam_enabled:
+            self._timer.stop()
+            self._is_cam_enabled = False
+            self.toggle_cam_button.setText("&Enable camera")
+        else:
+            self._timer.start()
+            self._is_cam_enabled = True
+            self.toggle_cam_button.setText("&Disable camera")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = SIFTApp()
+    window.show()
+    sys.exit(app.exec_())
